5dd186c708d8f01bc288ae5eea653ca980bd4c87
Eslam-Samir <eslamsamir232@gmail.com>
2016-03-17 21:40:05 +0200

    #6 - TODO add schedulers

--- src/controllers/ProcessSceneController.java
+++ src/controllers/ProcessSceneController.java
@@ -2,25 +2,25 @@ package controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
-
-
-
-
-
-
-
+import custom.views.GanttChart.ExtraData;
+import custom.views.GanttChart;
 import scheduler.Process;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -28,12 +28,17 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
 
 public class ProcessSceneController implements Initializable {	
 	@FXML
 	private GridPane grid;
+	@FXML
+	private Label type;
+	@FXML
+	private Label number;
 	@FXML
 	private TextField processName;
 	@FXML
@@ -42,6 +47,9 @@ public class ProcessSceneController implements Initializable {
 	private TextField burst;
 	@FXML
 	private TextField priority;
+	@FXML
+	private Button add;
+	
 	@FXML
 	private TableView<Process> table;
 	@FXML
@@ -75,16 +83,97 @@ public class ProcessSceneController implements Initializable {
 		}
 		else
 		{
+			for(Process x: list)
+			{
+				if(x.getName().equals(processName.getText()))
+				{
+					System.out.print("Enter a New Process\n");
+					return;
+				}
+			}
 			Process process = new Process(processName.getText(), 
 					Integer.valueOf(burst.getText()), Integer.valueOf(arrivalTime.getText()));
 			list.add(process);
 			table.setItems(list);
+			if(list.size() == numberOfProcesses)
+			{
+				grid.setDisable(true);
+			}
 		}
+		processName.clear();
+		arrivalTime.clear();
+		burst.clear();
+		priority.clear();
 	}
 	
 	public void startScheduling(ActionEvent action)
 	{
-		
+		if(list.size() < this.numberOfProcesses)
+		{
+			System.out.print("Enter the missing processes\n");
+			return;
+		}
+		Stage stage = new Stage();
+		stage.setTitle("Gantt Chart");
+
+        final NumberAxis xAxis = new NumberAxis();
+        final CategoryAxis yAxis = new CategoryAxis();
+
+        //TODO style this
+        VBox root = new VBox();
+        Label label = new Label("average waiting time:  ");
+      
+        root.getChildren().add(label);
+        
+        final GanttChart<Number,String> chart = new GanttChart<Number,String>(xAxis,yAxis);
+        
+        xAxis.setLabel("");
+        xAxis.setTickLabelFill(Color.CHOCOLATE);
+        xAxis.setMinorTickCount(4);
+
+        yAxis.setLabel("");
+        yAxis.setTickLabelFill(Color.CHOCOLATE);
+        yAxis.setTickLabelGap(10);
+
+        chart.setTitle(this.SchedulerType);
+        chart.setLegendVisible(false);
+        chart.setBlockHeight(20);
+
+        while(!list.isEmpty())
+        {
+        	XYChart.Series series = new XYChart.Series();
+        	Process process = list.get(0);
+        	int i = 0;
+        	while(i < list.size())
+        	{
+        		Process x = list.get(i);
+        		if(x.getName().equals("idle"))
+        		{
+        			series.getData().add(new XYChart.Data(x.getArrivalTime()
+	        				, x.getName(), new ExtraData(x.getRunTime(), "status-red")));
+	        		list.remove(x);
+        		}
+        		else if(x.getName().equals(process.getName()))
+        		{
+	        		series.getData().add(new XYChart.Data(x.getArrivalTime()
+	        				, x.getName(), new ExtraData(x.getRunTime(), "status-green")));
+	        		list.remove(x);
+        		}
+        		else
+        		{
+        			i++;
+        		}
+        	}
+        	chart.getData().add(series);
+        }   
+
+        root.getStylesheets().add(getClass().getResource("/resources/ganttchart.css").toExternalForm());
+        
+        root.getChildren().add(chart);
+        Scene scene  = new Scene(root,620,350);
+        
+        stage.setScene(scene);
+        stage.show();
 	}
 	
 	public void goBack(ActionEvent action) throws IOException
@@ -104,6 +193,10 @@ public class ProcessSceneController implements Initializable {
 	    {
 			list.remove(table.getSelectionModel().getSelectedIndex());
 			table.setItems(list);
+			if(grid.isDisabled())
+			{
+				grid.setDisable(false);
+			}
 	    }
 	}
 	
@@ -113,6 +206,7 @@ public class ProcessSceneController implements Initializable {
 	}
 	public void setNumberOfProcesses(int numberOfProcesses) {
 		this.numberOfProcesses = numberOfProcesses;		
+		number.setText(number.getText() + String.valueOf(numberOfProcesses));
 	}
 	public String getSchedulingType() {
 		return SchedulerType;
@@ -127,8 +221,9 @@ public class ProcessSceneController implements Initializable {
 			grid.getChildren().remove(6);
 			grid.getChildren().remove(6);
 		}
+		type.setText(type.getText() + schedulingType);
+		
 	}
-
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
