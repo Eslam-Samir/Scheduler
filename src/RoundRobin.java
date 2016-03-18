cbdc84c5f347e00731653801c526211f1d3316cc
manarsherif <manar.sherif93@gmail.com>
2016-03-18 20:32:37 +0200

    round robin

--- src/RoundRobin.java
+++ src/RoundRobin.java
@@ -38,10 +38,24 @@ public class RoundRobin extends Schedular
 			if(processes.get(i).getArrivalTime()>time){
 				double idleTime=processes.get(i).getArrivalTime()-time;
 				Process idle=new Process("idle",idleTime,time);
-				processes.add(i-1, idle);
+				processes.add(i, idle);
 				time+=idleTime;
 			}
 			else{
+				time+=processes.get(i).getRunTime();
+			}
+		}
+		double ttime=0;
+		for(int i=0;i<processes.size();i++){
+			if(processes.get(i).getRunTime()<q){
+			  double idleTime=q-processes.get(i).getRunTime();
+			  Process idle=new Process("idle",idleTime,ttime+processes.get(i).getRunTime());
+			  processes.add(i+1, idle);
+			  i++;
+			  time+=q;
+			  
+			}
+			else {
 				time+=processes.get(i).getRunTime();
 			}
 		}
