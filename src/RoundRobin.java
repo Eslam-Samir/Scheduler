2ddb7b1fdb4dc795f3db8f23f5dfed54432ee368
manarsherif <manar.sherif93@gmail.com>
2016-03-18 23:48:11 +0200

    infinite loop

--- src/RoundRobin.java
+++ src/RoundRobin.java
@@ -18,23 +18,22 @@ public class RoundRobin extends Schedular
 		// TODO Auto-generated method stub
 		return 0;
 	}
+	private int getFirstIdle(LinkedList<Process> processes)
+	{
+		
+		for(Process process: processes)
+		{
+			if(process.getName() =="idle")
+			{
+				return processes.indexOf(process);
+			}
+		}
+		return -1;
+	
+	}	
 	
 	public LinkedList<Process> run()
 	{
-		
-		double remainderTime;
-		for(int i = 0; i < processes.size(); i++){
-			
-			if (processes.get(i).getRunTime()>q){
-				remainderTime=processes.get(i).getRunTime()-q; 
-				Process p=new Process(processes.get(i).getName(),remainderTime
-									,(i+1)*q);
-				//output.push
-				processes.addLast(p);
-				processes.get(i).setRunTime(q);
-			}
-			
-		}
 		Collections.sort(processes, new Comparator<Process>(){
 			   @Override
 			   public int compare(Process o1, Process o2){
@@ -49,24 +48,52 @@ public class RoundRobin extends Schedular
 			}); 
 			
 			
-		double time =0;
-		for(int i =0;i<processes.size();i++){
-			if(processes.get(i).getArrivalTime()>time){
-				double idleTime=processes.get(i).getArrivalTime()-time;
-				Process idle=new Process("idle",idleTime,time);
-				processes.add(i, idle);
-				time+=idleTime;
+		double remainderTime;
+		double time=0;
+		for(int i = 0; i < processes.size(); i++){
+			if (processes.get(i).getName()=="idle")continue;
 			
+			if (processes.get(i).getRunTime()>q){
+				
+				if(processes.get(i).getArrivalTime()<=time){
+					int firstIdle=getFirstIdle(processes);
+					remainderTime=processes.get(i).getRunTime()-q; 
+					Process p=new Process(processes.get(i).getName(),remainderTime
+										,(i+1)*q);
+					if(firstIdle !=-1){
+						processes.get(firstIdle).setRunTime(processes.get(firstIdle).getRunTime()-q);
+						
+						processes.add(firstIdle, p);
+						
+						processes.get(i).setRunTime(q);
+						time+=q;
+						
+					}
+					else {
+						processes.addLast(p);
+						processes.get(i).setRunTime(q);
+						time+=q;
+					}
+				}
+				else{
+						double idleTime=processes.get(i).getArrivalTime()-time;
+						Process idle=new Process("idle",idleTime,time);
+						processes.add(i, idle);
+						time+=idleTime;
+				}
+								
 			}
 			else{
 				time+=processes.get(i).getRunTime();
 			}
 			
 		}
+		
+	
 		double ttime=0;
 		for(int i=0;i<processes.size();i++){
 			if(processes.get(i).getRunTime()<q){
-				if (processes.get(i).getName()=="idle") continue ;
+			//	if (processes.get(i).getName()=="idle") continue ;
 			  double idleTime=q-processes.get(i).getRunTime();
 			  Process idle=new Process("idle",idleTime,ttime+processes.get(i).getRunTime());
 			  processes.add(i+1, idle);
@@ -86,3 +113,4 @@ public class RoundRobin extends Schedular
 		
 	  
 }
+
