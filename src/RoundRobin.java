e53ef8dd4905381d9bb1c1644dfb2777737a0316
manarsherif <manar.sherif93@gmail.com>
2016-03-18 21:25:07 +0200

    yamosahel

--- src/RoundRobin.java
+++ src/RoundRobin.java
@@ -1,5 +1,6 @@
+import java.util.Collections;
 import java.util.LinkedList;
-
+import java.util.Comparator;
 
 public class RoundRobin extends Schedular
 {
@@ -28,11 +29,26 @@ public class RoundRobin extends Schedular
 				remainderTime=processes.get(i).getRunTime()-q; 
 				Process p=new Process(processes.get(i).getName(),remainderTime
 									,(i+1)*q);
+				//output.push
 				processes.addLast(p);
 				processes.get(i).setRunTime(q);
 			}
 			
 		}
+		Collections.sort(processes, new Comparator<Process>(){
+			   @Override
+			   public int compare(Process o1, Process o2){
+			        if(o1.getArrivalTime() < o2.getArrivalTime()){
+			           return -1; 
+			        }
+			        if(o1.getArrivalTime() > o2.getArrivalTime()){
+			           return 1; 
+			        }
+			        return 0;
+			   }
+			}); 
+			
+			
 		double time =0;
 		for(int i =0;i<processes.size();i++){
 			if(processes.get(i).getArrivalTime()>time){
@@ -40,10 +56,12 @@ public class RoundRobin extends Schedular
 				Process idle=new Process("idle",idleTime,time);
 				processes.add(i, idle);
 				time+=idleTime;
+			
 			}
 			else{
 				time+=processes.get(i).getRunTime();
 			}
+			
 		}
 		double ttime=0;
 		for(int i=0;i<processes.size();i++){
